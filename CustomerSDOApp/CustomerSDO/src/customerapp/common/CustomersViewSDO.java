9f3887b1ba5d46b3ec5eed08b82f9732e53c30ee
Jasim Aboobacker <jasim.aboobacker@gmail.com>
2015-03-11 22:12:50 +0530

    Initial Commit

---
+++ CustomerSDOApp/CustomerSDO/src/customerapp/common/CustomersViewSDO.java
@@ -0,0 +1,23 @@
+package customerapp.common;
+
+public interface CustomersViewSDO extends java.io.Serializable {
+
+   public java.lang.String getCustomerId();
+
+   public void setCustomerId(java.lang.String value);
+
+   public java.lang.String getCustomerName();
+
+   public void setCustomerName(java.lang.String value);
+
+   public java.lang.String getLastOrderNumber();
+
+   public void setLastOrderNumber(java.lang.String value);
+
+   public java.math.BigDecimal getLastOrderAmount();
+
+   public void setLastOrderAmount(java.math.BigDecimal value);
+
+
+}
+
